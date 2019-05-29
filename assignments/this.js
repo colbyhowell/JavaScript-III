b2f1d76a7582151c1914d0fb593c6445cfb0f787
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-05-28 21:57:46 -0500

    Answered the questions about this. Working on examples now

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding is when the "this" keyword is not bound to any particular object and is operating on the window scope, which is not recommended.
+* 2. Implicit Binding is when the "this" keyword is bound to object that is using a function. The "this" is the argument passed into the function.
+* 3. New Binding is when a new object is being created by a Constructor function. The "New" keyword is referring to the new object keys.
+* 4. Explicit Binding is when a function is being called, but the argument is overridden by the this keyword, which is passed through the parameter.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,8 @@
 // Principle 1
 
 // code example for Window Binding
+
+
 
 // Principle 2
 
